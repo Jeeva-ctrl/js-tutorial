6c358ceb8880a316d037e648728145593eb88b26
Jeeva-ctrl <jeevabharathy66@gmail.com>
2021-09-06 18:37:14 +0530

    1 file added

---
+++ questions.js
@@ -0,0 +1,48 @@
+/*
+
+1. What are the different data types present in javascript?
+
+Primitive Types : 
+
+String , BitInt , Boolean ,Undefined  , 
+
+Non-primitive types - Object - to store multiple types
+
+2. Explain Hoisting in javascript.
+Hoisting is a default behaviour of javascript where all the variable and function declarations are moved on top.
+
+This means that irrespective of where the variables and functions are declared, they are moved on top of the scope. The scope can be both local and global.
+
+3. Difference between “ == “ and “ === “ operators.
+
+Both are comparison operators. The difference between both the operators is that,“==” is used to compare values whereas, “ === “ is used to compare both value and types.
+
+3. Difference between “ == “ and “ === “ operators.
+
+Both are comparison operators. The difference between both the operators is that,“==” is used to compare values whereas, “ === “ is used to compare both value and types.
+
+5. Is javascript a statically typed or a dynamically typed language?
+
+JavaScript is a dynamically typed language. In a dynamically typed language, the type of a variable is checked during run-time in contrast to statically typed language, where the type of a variable is checked during compile-time.
+
+
+What is NaN property in JavaScript?
+NaN property represents “Not-a-Number” value. It indicates a value which is not a legal number.
+
+typeof of a NaN will return a Number .
+
+To check if a value is NaN, we use the isNaN() function,
+
+8. What is an Immediately Invoked Function in JavaScript?
+An Immediately Invoked Function ( known as IIFE and pronounced as IIFY) is a function that runs as soon as it is defined.
+
+Syntax of IIFE :
+
+(function(){ 
+  // Do something;
+})();
+
+
+
+*/
+
