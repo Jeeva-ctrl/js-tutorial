7d0d7e8733ab734a30de6f2fb6de57ab0ee8fde0
Jeeva-ctrl <jeevabharathy66@gmail.com>
2021-09-13 19:08:00 +0530

    1 file modified

--- angular.js
+++ angular.js
@@ -146,6 +146,35 @@ gOnDestroy( ) It gets called just before Angular destroys the component. This ho
 
 ==================================================================================================================================================
 
+8. Explain string interpolation and property binding in Angular.
+
+String interpolation and property binding are parts of data-binding in Angular.
+
+Data-binding is a feature in angular, which provides a way to communicate between the component(Model) and its view(HTML template).
+Data-binding can be done in two ways, one-way binding and two-way binding.
+
+In Angular, data from the component can be inserted inside the HTML template. In one-way binding, any changes in the component will directly reflect inside the HTML template but, vice-versa is not possible. Whereas, it is possible in two-way binding.
+
+String interpolation and property binding allow only one-way data binding.
+
+String interpolation uses the double curly braces {{ }} to display data from the component. Angular automatically runs the expression written inside the curly braces, 
+for example, {{ 2 + 2 }} will be evaluated by Angular and the output 4, will be displayed inside the HTML template. Using property binding, we can bind the DOM properties of an HTML element to a component's property. Property binding uses the square brackets [ ] syntax.
+
+
+@Component({
+  selector: 'my-app',
+  template: `
+            <h3>{{ "Using String Interpolation: " + title }}</h3>
+            <h3 [textContent]="title"></h3>
+            {{title}}
+            `,
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  title = 'Angular 4';
+}
+
+
 
 ==================================================================================================================================================
 
