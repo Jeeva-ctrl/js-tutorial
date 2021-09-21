b0e3186f376d071d35cdff7366913300941a13a4
Jeeva-ctrl <jeevabharathy66@gmail.com>
2021-09-21 08:11:56 +0530

    1 file added

---
+++ Core/c#.js
@@ -0,0 +1,14 @@
+/*
+
+Characteristics of C# var keyword
+
+ var keyword initializes variables with var support.
+ 
+  The data type of a var variable will be determined when assigning values to variables. 
+  
+  The following program will automatically cast the variable. And of course, after assigning the variable value, the variable has a defined data type and cannot be replaced. I
+
+
+  
+
+*/
