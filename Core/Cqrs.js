c8dce3c8663b4f4b31190d065ade96769084c5ec
Jeeva-ctrl <jeevabharathy66@gmail.com>
2021-09-21 08:13:35 +0530

    1 file modified

--- Core/Cqrs.js
+++ Core/Cqrs.js
@@ -7,7 +7,10 @@ Implementing CQRS in your application can maximize its performance, scalability,
 
 The flexibility created by migrating to CQRS allows a system to better evolve over time and prevents update commands from causing merge conflicts at the domain level.
 
+Disadvantages of the CQRS Pattern
 
+It adds unnecessary complexity if applications have simple CRUD operations, which can be achieved by traditional architectural styles.
 
+As we require separate models for read & write, code duplication is inevitable.
 
 */
