44bedd72d3f241b146867d52be2f6e5ea526e196
Jeeva-ctrl <jeevabharathy66@gmail.com>
2021-09-21 08:00:17 +0530

    1 file added

---
+++ Azure/azurefunction.js
@@ -0,0 +1,39 @@
+/*
+
+Azure functions is a serverless concept of cloud native design that allows a piece of code deployed and execute without any need of server infrastructure, web server, or any configurations. 
+
+Azure functions can be written in multiple languages such as C#, Java, JavaScript, TypeScript, and Python.
+
+Azure functions are scalable. 
+
+When demand of execution increases, more resources are allocated automatically to the service and when requests fall, all extra resources and application instances drop off automatically
+
+Azure functions are best suited for smaller apps have events that can work independently of other websites. 
+
+Some of the common azure functions are sending emails, starting backup, order processing, task scheduling such as database cleanup, sending notifications, messages, and IoT data processing
+
+Azure functions are lightweight and serverless.
+
+Azure functions are easier to write and deploy.
+
+Azure functions are fast to execute because there is no large application, startup time, initialization, and other events fired before the code is executed.
+
+Azure functions’ execution is triggered when an event is fired.
+
+Azure functions are compute-on-demand and that is scalable. When demand of execution increases, more resources are allocated automatically to the service and when requests fall, all extra resources and application instances drop off automatically.
+
+Azure functions support multiple programming languages including C#, F#, Java, JavaScript, TypeScript, and Python. You choose your choice of language.
+
+Azure functions do not need any infrastructure and have 0 maintenance.
+
+Azure function can be build, tested, and deployed in Azure portal using a browser.
+
+Azure functions are easy to upgrade and doesn’t affect other parts of the website.
+
+Azure functions use industry standard and can communicate with other APIs, databases, and libraries.
+
+
+Azure functions in Azure is created using Functions App
+
+
+*/
