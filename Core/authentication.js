df591d8d0cef93745b77da67bbffab1bf2bc49c4
Jeeva-ctrl <jeevabharathy66@gmail.com>
2021-09-24 21:53:41 +0530

    1 file modified

--- Core/authentication.js
+++ Core/authentication.js
@@ -18,18 +18,30 @@ services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, options => Configuration.Bind("CookieSettings", options));
 
 
-services.AddAuthentication(options =>
-{
-	options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-.AddJwtBearer(options =>
-{
-	options.Authority = "https://{yourOktaDomain}/oauth2/default";
-	options.Audience = "api://default";
-	options.RequireHttpsMetadata = false;
-});
+public void ConfigureServices(IServiceCollection services)    
+{    
+    services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)    
+    .AddJwtBearer(options =>    
+    {    
+        options.TokenValidationParameters = new TokenValidationParameters    
+        {    
+            ValidateIssuer = true,    
+            ValidateAudience = true,    
+            ValidateLifetime = true,    
+            ValidateIssuerSigningKey = true,    
+            ValidIssuer = Configuration["Jwt:Issuer"],    
+            ValidAudience = Configuration["Jwt:Issuer"],    
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))    
+        };    
+    });    
+    services.AddMvc();    
+} 
 
-app.UseAuthentication();
+public void Configure(IApplicationBuilder app, IHostingEnvironment env)    
+{    
+    app.UseAuthentication();    
+    app.UseMvc();    
+} 
 
 [Authorize]
 [Route("api/[controller]")]
@@ -38,23 +50,63 @@ public class ValuesController : Controller
 	...
 }
 
+private string GenerateJSONWebToken(UserModel userInfo)    
+        {    
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));    
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);    
+    
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],    
+              _config["Jwt:Issuer"],    
+              null,    
+              expires: DateTime.Now.AddMinutes(120),    
+              signingCredentials: credentials);    
+    
+            return new JwtSecurityTokenHandler().WriteToken(token);    
+        } 
+
 
 Introduction to cookie authentication
 Cookie authentication in ASP.NET Core web application is the popular choice for developers to implement authentication in most customer-facing web applications and is also easy to implement in ASP.NET Core as it is provided out of the box without the need to reference any additional NuGet packages.
 
 ASP.NET Core provides a cookie authentication mechanism which on login serializes the user details in form of claims into an encrypted cookie and then sends this cookie back to the server on subsequent requests which gets validated to recreate the user object from claims and sets this user object in the HttpContext so that it is available & is valid only for that request.
 
+      [HttpPost]  
+    public async Task < IActionResult > Login(LoginModel objLoginModel) {  
+        if (ModelState.IsValid) {  
+            var user = users.Where(x => x.Username == objLoginModel.UserName && x.Password == objLoginModel.Password).FirstOrDefault();  
+            if (user == null) {  
+                //Add logic here to display some message to user    
+                ViewBag.Message = "Invalid Credential";  
+                return View(objLoginModel);  
+            } else {  
+                //A claim is a statement about a subject by an issuer and    
+                //represent attributes of the subject that are useful in the context of authentication and authorization operations.    
+                var claims = new List < Claim > () {  
+                    new Claim(ClaimTypes.NameIdentifier, Convert.ToString(user.UserId)),  
+                        new Claim(ClaimTypes.Name, user.Username),  
+                        new Claim(ClaimTypes.Role, user.Role),  
+                        new Claim("FavoriteDrink", "Tea")  
+                };  
+                //Initialize a new instance of the ClaimsIdentity with the claims and authentication scheme    
+                var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);  
+                //Initialize a new instance of the ClaimsPrincipal with ClaimsIdentity    
+                var principal = new ClaimsPrincipal(identity);  
+                //SignInAsync is a Extension method for Sign in a principal for the specified scheme.    
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal, new AuthenticationProperties() {  
+                    IsPersistent = objLoginModel.RememberLogin  
+                });  
+                return LocalRedirect(objLoginModel.ReturnUrl);  
+            }  
+        }  
+        return View(objLoginModel);  
+    } 
 
-await HttpContext.SignInAsync(
-                CookieAuthenticationDefaults.AuthenticationScheme,
-                new ClaimsPrincipal(claimsIdentity),
-                authProperties);
-
-
-
-
-await context.HttpContext.SignOutAsync(
-                CookieAuthenticationDefaults.AuthenticationScheme);
+     public async Task < IActionResult > LogOut() {  
+        //SignOutAsync is Extension method for SignOut    
+        await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);  
+        //Redirect to home page    
+        return LocalRedirect("/");  
+    }  
 
                 
 */
