289eca1bd34e10f27006e37a039368f4f41e7db7
Jeeva-ctrl <jeevabharathy66@gmail.com>
2021-10-01 12:21:28 +0530

    1 file added

---
+++ Core/Design Patterns/Creational Pattern/FactoryPattern.js
@@ -0,0 +1,154 @@
+/*
+
+=============================================================================================================================
+
+Factory method is a creational design pattern which solves the problem of creating product objects without specifying their concrete classes.
+
+
+Factory Method defines a method, which should be used for creating objects instead of direct constructor call (new operator). Subclasses can override this method to change the class of objects that will be created.
+
+
+=============================================================================================================================
+
+sing System;
+namespace Factory
+{
+ /// <summary>
+ /// The 'Product' interface
+ /// </summary>
+ public interface IFactory
+ {
+ void Drive(int miles);
+ }
+
+ /// <summary>
+ /// A 'ConcreteProduct' class
+ /// </summary>
+ public class Scooter : IFactory
+ {
+ public void Drive(int miles)
+ {
+ Console.WriteLine("Drive the Scooter : " + miles.ToString() + "km");
+ }
+ }
+
+ /// <summary>
+ /// A 'ConcreteProduct' class
+ /// </summary>
+ public class Bike : IFactory
+ {
+ public void Drive(int miles)
+ {
+ Console.WriteLine("Drive the Bike : " + miles.ToString() + "km");
+ }
+ }
+
+ /// <summary>
+ /// The Creator Abstract Class
+ /// </summary>
+ public abstract class VehicleFactory
+ {
+ public abstract IFactory GetVehicle(string Vehicle);
+
+ }
+
+ /// <summary>
+ /// A 'ConcreteCreator' class
+ /// </summary>
+ public class ConcreteVehicleFactory : VehicleFactory
+ {
+ public override IFactory GetVehicle(string Vehicle)
+ {
+ switch (Vehicle)
+ {
+ case "Scooter":
+ return new Scooter();
+ case "Bike":
+ return new Bike();
+ default:
+ throw new ApplicationException(string.Format("Vehicle '{0}' cannot be created", Vehicle));
+ }
+ }
+
+ }
+ 
+ /// <summary>
+ /// Factory Pattern Demo
+ /// </summary>
+ class Program
+ {
+ static void Main(string[] args)
+ {
+ VehicleFactory factory = new ConcreteVehicleFactory();
+
+ IFactory scooter = factory.GetVehicle("Scooter");
+ scooter.Drive(10);
+
+ IFactory bike = factory.GetVehicle("Bike");
+ bike.Drive(20);
+
+ Console.ReadKey();
+
+ }
+ }
+}
+=============================================================================================================================
+
+When to use it?
+
+Subclasses figure out what objects should be created.
+
+Parent class allows later instantiation to subclasses means the creation of an object is done when it is required.
+
+The process of objects created is required to centralize within the application.
+
+A class (creator) will not know what classes it will be required to create.
+
+=============================================================================================================================
+
+
+
+
+=============================================================================================================================
+
+
+
+=============================================================================================================================
+
+
+=============================================================================================================================
+
+=============================================================================================================================
+
+
+
+=============================================================================================================================
+
+
+
+
+=============================================================================================================================
+
+
+
+=============================================================================================================================
+
+
+=============================================================================================================================
+
+=============================================================================================================================
+
+
+
+=============================================================================================================================
+
+
+
+
+=============================================================================================================================
+
+
+
+=============================================================================================================================
+
+*/
